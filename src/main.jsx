1d6520f39b28cfdfd27fb788137c738e7d9ffd80
Koushik Brandmirchi <koushik@brandmirchi.com>
2026-03-24 15:09:07 +0530

    WIP Dom set

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { AppProvider } from './context/AppContext';
 import App from './App';
@@ -22,12 +22,12 @@ document.head.appendChild(style);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <AppProvider>
           <App />
         </AppProvider>
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
